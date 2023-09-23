c72b7f29aabbc826c8185398ee0948ad7f1026bd
cel017 <64001922+cel017@users.noreply.github.com>
2023-09-22 23:46:45 -0600

    fixed section scraping bug

--- src/course.py
+++ src/course.py
@@ -1,22 +1,11 @@
 class Course:
-    def __init__(self, code: str, name: str, descr: str, lecs = [], labs = [], seminars = []):
+    def __init__(self, code: str, name: str, descr: str, lecs: list, labs: list, seminars: list):
         self.code = code
         self.name = name
         self.descr = descr
         self.lecs = lecs
         self.labs = labs
         self.seminars = seminars
-    
-    def __repr__(self):
-        cmb = lambda X: ''.join(map(str, X))
-        return f"{self.name}\n\n{self.descr}\n\nLectures:\
-            \n{cmb(self.lecs)}\nLabs:\n{cmb(self.labs)}\nSeminars:\n{cmb(self.seminars)}"
-
-    def to_dict(self):
-        return {"code": self.code, "name": self.name, "descr": self.descr, \
-                "lecs": [i.__dict__ for i in self.lecs], \
-                "labs": [i.__dict__ for i in self.labs], \
-                "seminars": [i.__dict__ for i in self.seminars]}
 
     def parse_descr(self):
         pass
@@ -27,11 +16,6 @@ class Component:
         self.capacity = capacity
         self.duration = duration  
         self.instructors = instructors
-
-        self.finals = None
-    
-    def __repr__(self):
-        return "\t|\t".join(self.__dict__.values())+"\n"
     
     def set_finals(self):
         pass
--- src/scraper.py
+++ src/scraper.py
@@ -1,9 +1,11 @@
+from course import *
+
 import re
 import json
 import requests
-from course import *
 from pathlib import Path
 from bs4 import BeautifulSoup, SoupStrainer
+
 
 # TODO -> command line args for required data
 def scrape_all():
@@ -14,12 +16,13 @@ def scrape_all():
 
     for f in ("links.json",):
         assert f in dpath, f"{f} file missing from data directory"
+        
     # read links from links.json file
     with open(dpath["links.json"]) as f:
         links = json.load(f)
     
     # scrape sections
-    if "subjects.json" in dpath:
+    if "subjects.json" not in dpath:
         print("Scraping Subjects...")
         subjects = scrape_subjects(links["subjects"])
         with open(data_dir/"subjects.json", "w") as f:
@@ -29,14 +32,15 @@ def scrape_all():
         with open(dpath["subjects.json"]) as f:
             subjects = json.load(f)
 
-    print(f"({len(subjects)}) Subjects Found.\n")
+    print(f"({len(subjects)}) Subjects Scraped.\n")
     
     # scrape courses    
     secs_scraped = False   
     if "courses.json" not in dpath:
         print("Scraping Courses...")
         courses = scrape_courses(links["subjects"], subjects.keys())
-        with open(data_dir/"courses.json", "w") as f:
+        dpath["courses.json"] = data_dir/"courses.json"
+        with open(dpath["courses.json"], "w") as f:
             json.dump({"secs_scraped": False, "data": [crs.__dict__ for crs in courses]}, f, indent=4)
     else:
         with open(dpath["courses.json"]) as f:
@@ -44,13 +48,14 @@ def scrape_all():
             courses_json = json.load(f)
         secs_scraped = courses_json["secs_scraped"]
         # assumes order is preserved (py3.7> dict) ->
-        # attr order issues if file is manipulated by a script
-        if not secs_scraped:
+        # might cause attr order issues if file is manipulated elsewhere
+        if secs_scraped:
             courses = [Course(*crs.values()) for crs in courses_json["data"]]
         else:
             courses = []
             for crs in courses_json["data"]:
                 init_args = list(crs.values())
+                print(init_args)
                 # builtin dtypes
                 std_args = init_args[:3]
                 comp_args_raw = [c for c in init_args[3:]]
@@ -60,24 +65,26 @@ def scrape_all():
                     comp_args.append([Component(*c.values()) for c in comp_list])
                 all_args = std_args+comp_args
                 courses.append(Course(*all_args))
-    print(f"({len(courses)}) Courses Found.\n")
+    print(f"({len(courses)}) Courses Scraped.\n")
     
     # scrape sections
     if not secs_scraped:
+        # TODO <- Refactor to use a generator func for scrape sections;
+        # save time on already scraped data in case of crashes
         print("Scraping Sections...")
         scrape_sections(links["subjects"], courses)
-        with open(data_dir/"courses.json", "w") as f:
-            json.dump({"secs_scraped": True, "data": [crs.to_dict() for crs in courses]}, f, indent=4)
-        print("All Sections Found.")
+        dpath["courses.json"].unlink(missing_ok=False)
+        with open(dpath["courses.json"],  "w") as f:
+            json.dump({"secs_scraped": True, "data": [crs.__dict__ for crs in courses]}, f, indent=4)
+        print("All Sections Scraped.")
     else:
         print("Sections Found in Local File.")
 
     # scrape finals
     finals = get_finals_data(links["finals"])
 
-
 def scrape_subjects(url: str) -> dict[str: str]:
-    '''get a dict of all offered subject names with code keys
+    '''gets a dict of all offered subject names with code keys
     
     args:
         url(str): url to subject catalogue
@@ -93,7 +100,7 @@ def scrape_subjects(url: str) -> dict[str: str]:
     return subjects
 
 def scrape_courses(url: str, subjects: list) -> list[Course]:
-    '''get a list of Course objects
+    '''gets a list of Course objects
 
     args:
         url(str): url to subject catalogue
@@ -102,13 +109,14 @@ def scrape_courses(url: str, subjects: list) -> list[Course]:
     returns:
         (list[Course])
     '''
+
     courses = []
     for subj_code in subjects:
         # strain cards ignoring future courses; get href to course page
         crs_cards = BeautifulSoup(requests.get(f"{url}/{subj_code}").text, "lxml", \
         parse_only=SoupStrainer(class_="course first")).find_all(href=True)
 
-        i=0
+        i = 0
         while i < len(crs_cards):
             crs = crs_cards[i]
             crs_page=crs.get("href")
@@ -133,13 +141,13 @@ def scrape_courses(url: str, subjects: list) -> list[Course]:
                 # no descr => div clas="alert..."
                 crs_descr=""
             
-            courses.append(Course(crs_code, crs_name, crs_descr))
+            courses.append(Course(crs_code, crs_name, crs_descr, [], [], []))
             i+=1
-
+            
     return courses
 
 def scrape_sections(url: str, courses: list):
-    for crs in (courses):
+    for crs in courses:
         table_elems = BeautifulSoup(requests.get(f"{url}/{crs.code}").text, "lxml", \
         parse_only=SoupStrainer(class_="card-body")).find_all("table")
 
@@ -163,11 +171,17 @@ def scrape_sections(url: str, courses: list):
     return
 
 def get_finals_data(url: str):
-    # get the json file for finals schedule 
+    # gets the json file for finals schedule 
 
     # TODO <- if the request for finals_data is invalid, scrape the updated call from 
     # 'https://www.ualberta.ca/registrar/examinations/exam-schedules/fall-winter-exam-schedule.html'
     return requests.get(url).json()["data"]
+
+def clear():
+    data_dir = Path(__file__).parents[1]/"data"
+    for path in data_dir.iterdir():
+        if path.name == "links.json": continue
+        path.unlink()
 
 if __name__ == "__main__":
     scrape_all()
